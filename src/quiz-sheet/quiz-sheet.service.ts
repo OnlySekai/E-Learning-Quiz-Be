cfb0d534fc92bba7c09a9964a19a4fdc7db60935
onlysekai <minhtuan25902@gmail.com>
2024-04-22 00:17:23 +0700

    :+1: Refactor Be

--- src/quiz-sheet/quiz-sheet.service.ts
+++ src/quiz-sheet/quiz-sheet.service.ts
@@ -66,7 +66,7 @@ export class QuizSheetService {
 
   async getQuizSheet(
     sheetId: string,
-    omitKey = true,
+    omitKey = false,
   ): Promise<GetQuizSheetResponse> {
     const quizSheet: QuizAnswerSheet = await this.quizSheetModel
       .findById(sheetId)
